0d84e5a4de63a98f337b9129b05cdea5d08a4e39
896937848@qq.com <896937848@qq.com>
2022-06-28 22:00:27 +0800

    更新顺序表

--- stllib/SeqList.cpp
+++ stllib/SeqList.cpp
@@ -1,219 +1,303 @@
-/***********************************************************
- **文件名:      SeqList.cpp
- **功能描述:    顺序表操作类
- **创建日期:    2022-06-05
- **作者:       
- **版本:        0.1
- **---------------------------------------------------------
- ***********************************************************/
+/*****************************************************************************
+*  @copyright   
+*  @file        SeqList.cpp
+*  @brief       顺序表类
+*  @note
+*  
+*  @author      Snowgooc
+*  @version     1.0.0
+*  @date        2022-06-26
+*  
+*----------------------------------------------------------------------------
+*  Remark         : Description
+*----------------------------------------------------------------------------
+*  Change History :
+*  <Date>     | <Version> | <Author>       | <Description>
+*----------------------------------------------------------------------------
+*  
+*****************************************************************************/
 #include "SeqList.h"
 #include <string.h>
-#include <assert.h>
 
 
-/***********************************************************
- **函数名:      SeqList
- **输入:        uint32_c size -- 顺序表长度
- **输出:        
- **函数功能:    根据长度构造
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  根据容量构造
+ *  @param[in] size             顺序表长度
+ *  @note
+ *  
+ *  @return
+ *  
+ */
 template <class ET>
 SeqList<ET>::SeqList(uint32_c size)
 {
     this->length = 0;
     this->maxLength = size;
     this->elems = new ET[size];
-    assert(this->elems);
 }
 
-/***********************************************************
- **函数名:      SeqList
- **输入:        ET e[] -- 需拷贝顺序表
- **输入:        uint32_c n -- 需拷贝顺序表的长度
- **输入:        uint32_c size -- 顺序表长度
- **输出:        
- **函数功能:    将顺序表按长度复制到新的顺序表
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  顺序表按长度构造
+ *  @param[in] e[]          拷贝源顺序表
+ *  @param[in] n            拷贝源顺序表的长度
+ *  @param[in] size         顺序表容量
+ *  @note
+ *  
+ *  @return
+ *  
+ */
 template <class ET>
 SeqList<ET>::SeqList(ET e[], uint32_c n, uint32_c size)
 {
     this->length = n;
     this->maxLength = size;
     this->elems = new ET[size];
-    assert(this->elems);
-    memcpy(this->elems, e, sizeof(ET) * n);
+    memcpy(this->elems, e, sizeof(ET) *  n);
 }
 
-/***********************************************************
- **函数名:      SeqList
- **输入:        const SeqList<ET> &sl -- 顺序表类
- **输出:        
- **函数功能:    复制构造函数
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  复制构造
+ *  @param[in] sl           顺序表对象
+ *  @note
+ *  
+ *  @return
+ *  
+ */
 template <class ET>
 SeqList<ET>::SeqList(const SeqList<ET> &sl)
 {
     this->length = sl.length;
     this->maxLength = sl.maxLength;
     this->elems = new ET[sl.maxLength];
-    assert(this->elems);
-    memcpy(this->elems, sl.elems, sizeof(ET) * sl.length);
+    memcpy(this->elems, sl.elems, sizeof(ET) *  sl.length);
 }
 
-/***********************************************************
- **函数名:      ~SeqList
- **输入:        
- **输出:        
- **函数功能:    析构
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
-template <class ET>
-SeqList<ET>::~SeqList()
-{
-    DEL_ARRAY(this->elems)
-}
 
-/***********************************************************
- **函数名:      operator =
- **输入:        const SeqList<ET> &e -- 顺序表类对象
- **输出:        SeqList<ET>& -- 对象自身
- **函数功能:    赋值运算符重载
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+/**  
+ *  @brief  移动复制构造
+ *  @param[in] sl           顺序表对象
+ *  @note
+ *  
+ *  @return
+ *  
+ */
 template <class ET>
-SeqList<ET>& SeqList<ET>::operator =(const SeqList<ET> &sl)
+SeqList<ET>::SeqList(SeqList<ET> &&sl)
 {
     this->length = sl.length;
     this->maxLength = sl.maxLength;
-    // 删除旧表
-    DEL_ARRAY(this->elems)
-    // 再按照sl.maxLength的长度建表
+    this->elems = sl.elems;
+    sl.elems = nullptr;
+    sl.length = 0;
+    sl.maxLength = 0;
+}
+
+
+/**  
+ *  @brief  析构
+ *  @param[in]
+ *  @note
+ *  
+ *  @return
+ *  
+ */
+template <class ET>
+SeqList<ET>::~SeqList()
+{
+    DEL_ARRAY(this->elems);
+}
+
+
+/**  
+ *  @brief  赋值运算符
+ *  @param[in] sl           顺序表对象
+ *  @note
+ *                          
+ *  @return 
+ *  对象自身
+ *  
+ */
+template <class ET>
+SeqList<ET>& SeqList<ET>::operator=(const SeqList<ET> &sl)
+{
+    this->length = sl.length;
+    this->maxLength = sl.maxLength;
+    DEL_ARRAY(this->elems);
     this->elems = new ET[sl.maxLength];
-    assert(this->elems);
-    memcpy(this->elems, sl.elems, sizeof(ET) * sl.length);
+    memcpy(this->elems, sl.elems, sizeof(ET) *  sl.length);
     return *this;
 }
 
-/***********************************************************
- **函数名:      clear
- **输入:        
- **输出:        
- **函数功能:    清除数据
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  移动赋值运算符
+ *  @param[in] sl           顺序表对象
+ *  @note
+ *                          
+ *  @return 
+ *  对象自身
+ *  
+ */
+template <class ET>
+SeqList<ET>& SeqList<ET>::operator=(SeqList<ET> &&sl)
+{
+    this->length = sl.length;
+    this->maxLength = sl.maxLength;
+    DEL_ARRAY(this->elems);
+    this->elems = sl.elems;
+    sl.elems = nullptr;
+    sl.length = 0;
+    sl.maxLength = 0;
+    return *this;
+}
+
+
+/**  
+ *  @brief  下标运算符
+ *  @param[in] i           下标
+ *  @note
+ *                          
+ *  @return 
+ *  指向该元素的引用
+ *  
+ */
+template <class ET>
+ET& SeqList<ET>::operator[](const uint32_c i)
+{
+    return at(i);
+}
+
+
+/**  
+ *  @brief  清除数据
+ *  @param[in]
+ *  @note
+ *  
+ *  @return
+ *  
+ */
 template <class ET>
 void SeqList<ET>::clear()
 {
     length = 0;
 }
 
-/***********************************************************
- **函数名:      isEmpty
- **输入:        
- **输出:        bool -- true 为空; false 不为空
- **函数功能:    是否为空
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  是否为空
+ *  @param[in]
+ *  @note
+ *  
+ *  @retval true    -- 为空
+ *  @retval false   -- 不为空
+ *  
+ */
 template <class ET>
 bool SeqList<ET>::isEmpty() const
 {
     return (0 == length);
 }
 
-/***********************************************************
- **函数名:      getLength
- **输入:        
- **输出:        uint32_c -- 长度
- **函数功能:    获取长度
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  获取长度
+ *  @param[in]
+ *  @note
+ *  
+ *  @return 
+ *  长度
+ *  
+ */
 template <class ET>
 uint32_c SeqList<ET>::getLength() const
 {
     return length;
 }
 
-/***********************************************************
- **函数名:      getCapacity
- **输入:        
- **输出:        uint32_c -- 容量
- **函数功能:    获取容量
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  获取容量
+ *  @param[in]
+ *  @note
+ *  
+ *  @return 
+ *  容量
+ *  
+ */
 template <class ET>
 uint32_c SeqList<ET>::getCapacity() const
 {
     return maxLength;
 }
 
-/***********************************************************
- **函数名:      getElem
- **输入:        uint32_c i -- 位置
- **输入(输出):  ET &e -- 读取到的元素值
- **输出:        bool -- true 成功; false 是吧
- **函数功能:    按位置读元素
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  按下标读元素
+ *  @param[in] i            下标
+ *  @note
+ *  
+ *  @warning 需判断i是否越界
+ *  @return 
+ *  指向该元素的引用
+ *  
+ */
 template <class ET>
-bool SeqList<ET>::getElem(uint32_c i, ET &e) const
+ET& SeqList<ET>::at(uint32_c i) const
+{
+    return elems[i];
+}
+
+
+/**  
+ *  @brief  按位置读元素
+ *  @param[in] i           位置
+ *  @note
+ *  
+ *  @retval nullptr -- 获取失败
+ *  @retval const*  -- 指向该元素常量的指针
+ *  
+ */
+template <class ET>
+const ET* SeqList<ET>::getElem(uint32_c i) const
+{
+    if (0 == i || i > length)
+        return nullptr;
+    return &at(i - 1);
+}
+
+
+/**  
+ *  @brief  按位置修改元素
+ *  @param[in] e           数据源
+ *  @param[in] i           位置
+ *  @note
+ *  
+ *  @retval true    -- 修改成功
+ *  @retval false   -- 修改失败
+ *  
+ */
+template <class ET>
+bool SeqList<ET>::setElem(const ET &e, const uint32_c i)
 {
     if (0 == i || i > length) 
         return false;
-    e = elems[i - 1];
+    memcpy(&elems[i - 1], &e, sizeof(ET));
     return true;
 }
 
-/***********************************************************
- **函数名:      setElem
- **输入:        const ET &e -- 数据源
- **输入:        uint32_c i -- 位置
- **输出:        bool -- true 成功; false 是吧
- **函数功能:    按位置修改元素
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
-template <class ET>
-bool SeqList<ET>::setElem(const ET &e, uint32_c i)
-{
-    if (0 == i || i > length) 
-        return false;
-    elems[i - 1] = e;
-    return true;
-}
 
-/***********************************************************
- **函数名:      deleteElem
- **输入:        uint32_c i -- 位置
- **输出:        bool -- true 成功; false 是吧
- **函数功能:    删除该位置元素
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+/**  
+ *  @brief  删除该位置元素
+ *  @param[in] i           位置
+ *  @note
+ *  
+ *  @retval true    -- 删除成功
+ *  @retval false   -- 删除失败
+ *  
+ */
 template <class ET>
 bool SeqList<ET>::deleteElem(uint32_c i)
 {
@@ -221,38 +305,40 @@ bool SeqList<ET>::deleteElem(uint32_c i)
         return false;
     // 将数据往前移
     for (uint32_c j = i; j < length; j++)
-        elems[j - 1] = elems[j];
+        memcpy(&elems[j - 1], &elems[j], sizeof(ET));
     --length;
     return true;
 }
 
-/***********************************************************
- **函数名:      insert
- **输入:        const ET &e -- 数据源
- **输出:        bool -- true 成功; false 是吧
- **函数功能:    插入元素
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  插入元素
+ *  @param[in] e           数据源
+ *  @note
+ *  
+ *  @retval true    -- 插入成功
+ *  @retval false   -- 插入失败
+ *  
+ */
 template <class ET>
 bool SeqList<ET>::insert(const ET &e)
 {
     return insert(e, length + 1);
 }
 
-/***********************************************************
- **函数名:      insert
- **输入:        const ET &e -- 数据源
- **输入:        uint32_c i -- 位置
- **输出:        bool -- true 成功; false 是吧
- **函数功能:    按位置插入元素
- **作者:        
- **日期:        2022-06-05
- **版本:        0.1
- ***********************************************************/
+
+/**  
+ *  @brief  按位置插入元素
+ *  @param[in] e           数据源
+ *  @param[in] i           位置
+ *  @note
+ *  
+ *  @retval true    -- 插入成功
+ *  @retval false   -- 插入失败
+ *  
+ */
 template <class ET>
-bool SeqList<ET>::insert(const ET &e, uint32_c i)
+bool SeqList<ET>::insert(const ET &e, const uint32_c i)
 {
     if (0 == i || i > length + 1 || i > maxLength) 
         return false;
--- stllib/SeqList.h
+++ stllib/SeqList.h
@@ -1,11 +1,21 @@
-/***********************************************************
- **文件名:      SeqList.h
- **功能描述:    顺序表类
- **创建日期:    2022-06-05
- **作者:       
- **版本:        0.1
- **---------------------------------------------------------
- ***********************************************************/
+/*****************************************************************************
+*  @copyright   
+*  @file        SeqList.h
+*  @brief       顺序表类
+*  @note
+*  
+*  @author      Snowgooc
+*  @version     1.0.0
+*  @date        2022-06-26
+*  
+*----------------------------------------------------------------------------
+*  Remark         : Description
+*----------------------------------------------------------------------------
+*  Change History :
+*  <Date>     | <Version> | <Author>       | <Description>
+*----------------------------------------------------------------------------
+*  
+*****************************************************************************/
 #ifndef _STL_LIB_SEQLIST_H_
 #define _STL_LIB_SEQLIST_H_
 
@@ -20,54 +30,32 @@
 template <class ET>
 class SeqList
 {
-public:
-    // 根据长度构造顺序表
-    SeqList(uint32_c size = SL_MAX_LEN);
-
-    // 将顺序表按长度复制到新的顺序表
-    SeqList(ET e[], uint32_c n, uint32_c size = SL_MAX_LEN);
-
-    // 复制构造函数
-    SeqList(const SeqList<ET> &sl);
-
-    // 析构
-    virtual ~SeqList();
-
-    // 赋值运算符重载
-    SeqList<ET>& operator =(const SeqList<ET> &sl);
-
-public:
-    // 清除数据
-    void clear();
-
-    // 是否为空
-    bool isEmpty() const;
-
-    // 获取长度
-    uint32_c getLength() const;
-
-    // 获取容量
-    uint32_c getCapacity() const;
-
-    // 按位置读元素
-    bool getElem(uint32_c i, ET &e) const;
-
-    // 按位置修改元素
-    bool setElem(const ET &e, uint32_c i);
-
-    // 删除该位置元素
-    bool deleteElem(uint32_c i);
-
-    // 插入元素
-    bool insert(const ET &e);
-
-    // 按位置插入元素
-    bool insert(const ET &e, uint32_c i);
-
 protected:
-    uint32_c            length = 0;                     // 长度
-    uint32_c            maxLength = SL_MAX_LEN;         // 最大长度
-    ET                  *elems = nullptr;               // 顺序表
+    uint32_c            length = 0;                             // 长度
+    uint32_c            maxLength = SL_MAX_LEN;                 // 容量
+    ET                  *elems = nullptr;                       // 顺序表
+
+public:
+    SeqList(uint32_c size = SL_MAX_LEN);                        // 根据容量构造
+    SeqList(ET e[], uint32_c n, uint32_c size = SL_MAX_LEN);    // 顺序表按长度构造
+    SeqList(const SeqList<ET> &sl);                             // 复制构造
+    SeqList(SeqList<ET> &&sl);                                  // 移动复制构造
+    virtual ~SeqList();                                         // 析构
+    SeqList<ET>& operator=(const SeqList<ET> &sl);              // 赋值运算符
+    SeqList<ET>& operator=(SeqList<ET> &&sl);                   // 移动赋值运算符
+    ET& operator[](const uint32_c i);                           // 下标运算符
+
+public:
+    void clear();                                               // 清除数据
+    bool isEmpty() const;                                       // 是否为空
+    uint32_c getLength() const;                                 // 获取长度
+    uint32_c getCapacity() const;                               // 获取容量
+    ET& at(uint32_c i) const;                                   // 按下标读元素
+    const ET* getElem(uint32_c i) const;                        // 按位置读元素
+    bool setElem(const ET &e, const uint32_c i);                // 按位置修改元素
+    bool deleteElem(const uint32_c i);                          // 删除该位置元素
+    bool insert(const ET &e);                                   // 插入元素
+    bool insert(const ET &e, const uint32_c i);                 // 按位置插入元素
 
 };
 
